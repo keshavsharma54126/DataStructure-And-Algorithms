66fa332314d485f7105a0f173e009435447d14aa
unknown <sharmakeshav54126@gmail.com>
2023-04-07 05:19:27 +0530

    delete node function

--- circularLInikedList.cpp
+++ circularLInikedList.cpp
@@ -44,6 +44,34 @@ class Node{
         }
    }
 
+void deleteAtNode(Node* &tail,int element){
+    if(tail==NULL){
+        cout<<"there are no elements int the linked list"<<endl;
+    }
+    else{
+        Node* prev=tail;
+        Node* curr=prev->next;
+        while(curr->data!=element){
+            prev=prev->next;
+            curr=curr->next;
+        }
+        prev->next=curr->next;
+        //when there is onlty one element in the cll
+        if(curr==tail){
+            tail=prev;
+        }
+        //when there are two elements in the cll
+        if(curr==prev){
+            tail=NULL;
+            delete tail;
+            return;
+        }
+        curr->next=NULL;
+        delete curr;
+    }
+
+}
+
 void printCLL(Node* &tail){
     if(tail==NULL){
         cout<<"there are no nodes in this circular LInked list";
@@ -67,6 +95,12 @@ int main()
     insertAtNode(tail,7,6);
     insertAtNode(tail,8,7);
     insertAtNode(tail,9,7);
+    insertAtNode(tail,54,6);
+    printCLL(tail);
+    deleteAtNode(tail,54);
+    deleteAtNode(tail,6);
+    printCLL(tail);
+    deleteAtNode(tail,8);
     printCLL(tail);
 
     return 0;
